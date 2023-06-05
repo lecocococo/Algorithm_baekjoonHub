63a1be619bcbbe8af0697bd54adeb35ffdd1d853
LCM <62368629+lecocococo@users.noreply.github.com>
2023-06-06 00:25:55 +0900

    [Silver III] Title: 파일 정리, Time: 1016 ms, Memory: 94904 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+//        Map<String, Integer> ans = new TreeMap<>();
+        Map<String, Integer> ans = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            String[] temp= br.readLine().split("\\.");
+            if (ans.containsKey(temp[1])) {
+                ans.put(temp[1], ans.get(temp[1]) + 1);
+            } else {
+                ans.put(temp[1], 1);
+            }
+        }
+
+        List<String> ks = new ArrayList<>(ans.keySet());
+
+        Collections.sort(ks, (s1, s2) -> {
+            return s1.compareTo(s2);
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (String e : ks) {
+            sb.append(e).append(" ").append(ans.get(e)).append("\n");
+        }
+        System.out.println(sb);
+
+    }
+}
