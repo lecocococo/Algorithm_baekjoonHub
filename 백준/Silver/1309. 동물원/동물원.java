74fc675cc506e6749a2b8e38130d6bd893c0ec8d
LCM <62368629+lecocococo@users.noreply.github.com>
2023-06-13 01:41:52 +0900

    [Silver I] Title: 동물원, Time: 84 ms, Memory: 12312 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            dp[i] = 1;
+        }
+        dp[1] = 3;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = (2 * dp[i - 1] + dp[i - 2]) % 9901;
+        }
+
+        System.out.println(dp[N]);
+    }
+}
