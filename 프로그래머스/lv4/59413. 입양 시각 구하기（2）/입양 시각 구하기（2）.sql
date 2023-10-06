5d48999a5e06dd02aebd053855c4f9b4d05b3780
LCM <62368629+lecocococo@users.noreply.github.com>
2023-10-06 23:46:02 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,18 @@
+-- 코드를 입력하세요
+WITH RECURSIVE cte AS (
+    SELECT 0 AS num
+    UNION ALL
+    SELECT num+1
+    FROM cte
+    WHERE num < 23
+)
+
+SELECT cte.num, IFNULL(a.입양횟수, 0)
+FROM cte
+LEFT JOIN (SELECT HOUR(datetime) 시간대, COUNT(*) 입양횟수
+           FROM animal_outs
+           GROUP BY 시간대
+           ORDER BY 시간대) a
+ON cte.num = a.시간대;
+# SELECT HOUR(DATETIME)
+# FROM ANIMAL_OUTS;
